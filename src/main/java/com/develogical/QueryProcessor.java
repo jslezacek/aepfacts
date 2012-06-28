23b2a8da60937a08a6b67e9f1403b70adc22fa86
jslezacek <jozef@slezacek.ch>
2012-06-28 15:09:37 +0100

    Added response to 'largest'

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -3,17 +3,32 @@ package com.develogical;
 public class QueryProcessor {
 
     public String process(String query) {
+        System.out.println(query);
+        String response = "";
         if (query.contains("hi")) {
-            return "hello";
+            response = "hello";
         } else if (query.contains("what is your name")) {
-            return "A-Team";
+            response = "A-Team";
         } else if (query.startsWith("what is") && query.contains("plus")) {
             String[] parts = query.split(" ");
             int a = Integer.parseInt(parts[2]);
             int b = Integer.parseInt(parts[4]);
-            return new Integer(a + b).toString();
+            response = new Integer(a + b).toString();
+        } else if (query.contains("largest:")) {
+            int indexOfColon = query.indexOf(':');
+            String postColon = query.substring(indexOfColon + 1);
+            String[] numbers = postColon.split(",");
+            int maxInt = Integer.MIN_VALUE;
+            for (String numString : numbers) {
+                int num = Integer.parseInt(numString);
+                if (num > maxInt) {
+                    maxInt = num;
+                }
+            }
+            response = new Integer(maxInt).toString();
         }
-        return "";
+        System.out.println("Reponse: " + response);
+        return response;
     }
 
 }
